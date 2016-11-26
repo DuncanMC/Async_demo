a3fdc70fd0d35d1f0dbc68d993cd8a69e2447579
Duncan Champney <DChampney@wareto.com>
2016-11-25 20:32:50 -0500

    Moved DownloadManager.swift into proper folder.

---
+++ Async_demo/DownloadManager.swift
@@ -0,0 +1,50 @@
+//
+//  DownloadManager.swift
+//  Async_demo
+//
+//  Created by Duncan Champney on 11/25/16.
+//  Copyright © 2016 Duncan Champney. All rights reserved.
+//
+
+import Foundation
+
+
+typealias DataClosure = (Data?, Error?) -> Void
+
+/**
+ This class is a trivial example of a class that handles async processing. It offers a single function, `downloadFileAtURL()`
+ */
+class DownloadManager: NSObject {
+  
+  static var downloadManager = DownloadManager()
+  
+  private lazy var session: URLSession = {
+    return URLSession.shared
+  }()
+  
+  /**
+   This function demonstrates handling an async task.
+   - Parameter url The url to download
+   - Parameter completion: A completion handler to execute once the download is finished
+   */
+  
+  func downloadFileAtURL(_ url: URL, completion: @escaping DataClosure) {
+    
+    //We create a URLRequest that does not allow caching so you can see the download take place
+    let request = URLRequest(url: url,
+                             cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                             timeoutInterval: 30.0)
+    let dataTask = URLSession.shared.dataTask(with: request) {
+      data, response, error in
+      
+      //Perform the completion handler on the main thread
+      DispatchQueue.main.async {
+        //Call the copmletion handler that was passed to us
+        completion(data, error)
+      }
+    }
+    dataTask.resume()
+    
+    //When we get here the data task will NOT have completed yet!
+  }
+}
